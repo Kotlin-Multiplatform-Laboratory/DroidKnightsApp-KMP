91224337f13faa02c4d94aa13f4de359f426fc68
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-02-09 23:10:32 +0900

    [kmp-plugin] Add primitive plugins for android, ios

---
+++ build-logic/src/main/kotlin/com/droidknights/app/KotlinMultiplatform.kt
@@ -0,0 +1,23 @@
+package com.droidknights.app
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
+internal fun Project.configureKotlinMultiplatform() {
+    with(pluginManager) {
+        apply("org.jetbrains.kotlin.multiplatform")
+    }
+    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink>().configureEach {
+        notCompatibleWithConfigurationCache("Configuration chache not supported for a system property read at configuration time")
+    }
+}
+
+fun Project.kotlin(action: KotlinMultiplatformExtension.() -> Unit) {
+    extensions.configure(action)
+}
---
+++ build-logic/src/main/kotlin/com/droidknights/app/KotlinMultiplatformAndroid.kt
@@ -0,0 +1,34 @@
+package com.droidknights.app
+
+import com.android.build.gradle.TestExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+internal fun Project.configureKotlinMultiplatformAndroid() {
+    with(pluginManager) {
+        apply("com.android.library")
+    }
+
+    kotlin {
+        androidTarget {
+            compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    extensions.configure<TestExtension> {
+        configureKotlinAndroid()
+        sourceSets {
+            getByName("main") {
+                assets.srcDirs("src/androidMain/assets")
+                java.srcDirs("src/androidMain/kotlin", "src/commonMain/kotlin")
+                res.srcDirs("src/androidMain/res")
+            }
+            getByName("test") {
+                assets.srcDirs("src/androidUnitTest/assets")
+                java.srcDirs("src/androidUnitTest/kotlin", "src/commonTest/kotlin")
+                res.srcDirs("src/androidUnitTest/res")
+            }
+        }
+    }
+}
