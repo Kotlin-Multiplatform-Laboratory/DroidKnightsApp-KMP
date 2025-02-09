b30bf9adbdb5068942bd1635b78d4f11c2c42ad5
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-02-10 00:18:11 +0900

    [kmp-plugin] KMP Primitive plugin 정리

--- build-logic/src/main/kotlin/com/droidknights/app/KotlinMultiplatform.kt
+++ build-logic/src/main/kotlin/com/droidknights/app/KotlinMultiplatform.kt
@@ -3,18 +3,32 @@ package com.droidknights.app
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
-internal fun Project.configureKotlinMultiplatform() {
+internal fun Project.configureKotlinMultiplatform(
+    extension: KotlinMultiplatformExtension
+) = extension.apply {
+    jvmToolchain(17)
     with(pluginManager) {
         apply("org.jetbrains.kotlin.multiplatform")
     }
-    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
-        compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+    applyDefaultHierarchyTemplate()
+
+    sourceSets.apply {
+        commonMain {
+            dependencies {
+                implementation(libs.findLibrary("coroutine.core").get())
+                implementation(libs.findLibrary("kermit").get())
+            }
+        }
+        androidMain {
+            dependencies {
+                implementation(libs.findLibrary("coroutines-android").get())
+            }
+        }
     }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink>().configureEach {
-        notCompatibleWithConfigurationCache("Configuration chache not supported for a system property read at configuration time")
+        notCompatibleWithConfigurationCache("Configuration cache not supported for a system property read at configuration time")
     }
 }
 
--- build-logic/src/main/kotlin/com/droidknights/app/KotlinMultiplatformAndroid.kt
+++ build-logic/src/main/kotlin/com/droidknights/app/KotlinMultiplatformAndroid.kt
@@ -1,18 +1,33 @@
 package com.droidknights.app
 
+import com.android.build.api.dsl.LibraryExtension
 import com.android.build.gradle.TestExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-internal fun Project.configureKotlinMultiplatformAndroid() {
-    with(pluginManager) {
-        apply("com.android.library")
-    }
+internal fun Project.configureKotlinMultiplatformAndroid(
+    extension: LibraryExtension
+) = extension.apply {
+    compileSdk = libs.findVersion("android.compileSdk").get().requiredVersion.toInt()
 
     kotlin {
         androidTarget {
             compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    defaultConfig {
+        minSdk = libs.findVersion("android.minSdk").get().requiredVersion.toInt()
+        consumerProguardFiles("consumer-proguard-rules.pro")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 
