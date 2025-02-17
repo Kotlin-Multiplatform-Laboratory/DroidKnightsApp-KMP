54dc894464648f3aab0442abccdd6d033e487bbd
jihyunniiii <jita1125@naver.com>
2025-02-17 10:09:42 +0900

    Inject Ktorfit instance using DI

--- core/data/src/main/java/com/droidknights/app/core/data/di/ApiModule.kt
+++ core/data/src/main/java/com/droidknights/app/core/data/di/ApiModule.kt
@@ -4,6 +4,8 @@ import com.droidknights.app.core.data.api.GithubApi
 import com.droidknights.app.core.data.api.GithubRawApi
 import com.droidknights.app.core.data.api.createGithubApi
 import com.droidknights.app.core.data.api.createGithubRawApi
+import com.droidknights.app.core.data.di.qualifier.GitRaw
+import com.droidknights.app.core.data.di.qualifier.Github
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -31,23 +33,35 @@ internal object ApiModule {
 
     @Provides
     @Singleton
-    fun provideGithubApi(
+    @Github
+    fun provideGithubKtrofit(
         httpClient: HttpClient
-    ): GithubApi = Ktorfit.Builder()
+    ): Ktorfit = Ktorfit.Builder()
         .baseUrl("https://api.github.com/")
         .httpClient(client = httpClient)
         .build()
-        .createGithubApi()
+
+    @Provides
+    @Singleton
+    @GitRaw
+    fun provideGitRawKtorfit(
+        httpClient: HttpClient
+    ): Ktorfit = Ktorfit.Builder()
+        .baseUrl("https://raw.githubusercontent.com/")
+        .httpClient(client = httpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideGithubApi(
+        @Github ktorfit: Ktorfit
+    ): GithubApi = ktorfit.createGithubApi()
 
     @Provides
     @Singleton
     fun provideGitRawApi(
-        httpClient: HttpClient
-    ): GithubRawApi = Ktorfit.Builder()
-        .baseUrl("https://raw.githubusercontent.com/")
-        .httpClient(client = httpClient)
-        .build()
-        .createGithubRawApi()
+        @GitRaw ktorfit: Ktorfit
+    ): GithubRawApi = ktorfit.createGithubRawApi()
 
     @Provides
     @Singleton
---
+++ core/data/src/main/java/com/droidknights/app/core/data/di/qualifier/Qualifier.kt
@@ -0,0 +1,9 @@
+package com.droidknights.app.core.data.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class Github
+
+@Qualifier
+annotation class GitRaw
