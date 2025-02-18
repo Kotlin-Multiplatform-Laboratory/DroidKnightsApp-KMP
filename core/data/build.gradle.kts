4d6a79fcad45155147936074ee48e8995a6112b7
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-02-18 16:44:08 +0900

    Apply okio/ktorfit

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,27 +1,34 @@
 import com.droidknights.app.setNamespace
 
 plugins {
-    id("droidknights.android.library")
+    id("droidknights.kotlin.multiplatform")
+    id("droidknights.kotlin.koin")
+    id("droidknights.kotlin.serialization")
+    id("droidknights.kotlin.ktorfit")
     id("kotlinx-serialization")
-    alias(libs.plugins.ktorfit)
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.model)
+            implementation(projects.core.datastore)
+            implementation(projects.core.dataApi)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.serialization.json.okio)
+            implementation(libs.okio)
+        }
+        commonTest.dependencies {
+            implementation(libs.turbine)
+        }
+        iosMain.dependencies {
+            implementation(projects.core.model)
+            implementation(projects.core.datastore)
+            implementation(projects.core.dataApi)
+        }
+    }
 }
 
 android {
     setNamespace("core.data")
-}
-
-dependencies {
-    implementation(projects.core.model)
-    implementation(projects.core.datastore)
-    implementation(projects.core.dataApi)
-
-    implementation(libs.ktorfit.lib)
-
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.kotlinx.datetime)
-    testImplementation(libs.turbine)
 }
