3f983352001679900cc2b3f0f30aa02ffed9593c
LEE YOU BIN <youbeene3409@naver.com>
2025-03-02 02:16:38 +0900

    add Session UI to commonMain

---
+++ feature/session/src/commonMain/kotlin/com/droidknights/app/feature/session/navigation/SessionNavigation.kt
@@ -0,0 +1,41 @@
+package com.droidknights.app.feature.session.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.droidknights.app.core.model.Session
+import com.droidknights.app.core.navigation.Route
+import com.droidknights.app.feature.session.SessionDetailScreen
+import com.droidknights.app.feature.session.SessionScreen
+import com.droidknights.app.core.navigation.Route.Session as SessionRoute
+
+fun NavController.navigateSession() {
+    navigate(SessionRoute)
+}
+
+fun NavController.navigateSessionDetail(sessionId: String) {
+    navigate(Route.SessionDetail(sessionId))
+}
+
+fun NavGraphBuilder.sessionNavGraph(
+    onBackClick: () -> Unit,
+    onSessionClick: (Session) -> Unit,
+    onShowErrorSnackBar: (throwable: Throwable?) -> Unit,
+) {
+    composable<SessionRoute> {
+        SessionScreen(
+            onBackClick = onBackClick,
+            onSessionClick = onSessionClick,
+            onShowErrorSnackBar = onShowErrorSnackBar
+        )
+    }
+
+    composable<Route.SessionDetail> { navBackStackEntry ->
+        val sessionId = navBackStackEntry.toRoute<Route.SessionDetail>().sessionId
+        SessionDetailScreen(
+            sessionId = sessionId,
+            onBackClick = onBackClick
+        )
+    }
+}
