d4c78da85148edc40033176a181b49fd96aac618
LEE YOU BIN <youbeene3409@naver.com>
2025-03-02 18:30:23 +0900

    add session module dependencies

--- feature/session/build.gradle.kts
+++ feature/session/build.gradle.kts
@@ -3,6 +3,7 @@ import com.droidknights.app.setNamespace
 plugins {
     id("droidknights.kotlin.multiplatform")
     id("droidknights.compose.multiplatform")
+    id("droidknights.kotlin.koin")
 }
 
 android {
@@ -22,8 +23,14 @@ kotlin {
         }
         commonMain.dependencies {
             implementation(projects.core.model)
+            implementation(projects.core.designsystem)
+            implementation(projects.core.domain)
+            implementation(projects.core.navigation)
+            implementation(projects.core.ui)
 
+            implementation(compose.components.resources)
             implementation(libs.kotlinx.immutable)
+            implementation(libs.compose.navigation)
         }
         iosMain.dependencies {
 
