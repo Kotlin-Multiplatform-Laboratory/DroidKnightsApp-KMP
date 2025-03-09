7fadadeb74674470170ec4033b09e348a5d005b3
LEE YOU BIN <youbeene3409@naver.com>
2025-03-09 15:34:34 +0900

    make commonMain module in home module

---
+++ feature/home/src/commonMain/kotlin/com/droidknights/app/feature/home/HomeViewModel.kt
@@ -0,0 +1,42 @@
+package com.droidknights.app.feature.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.droidknights.app.core.domain.usecase.GetSponsorsUseCase
+import com.droidknights.app.feature.home.model.SponsorsUiState
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel
+class HomeViewModel(
+    private val getSponsorsUseCase: GetSponsorsUseCase,
+) : ViewModel() {
+    private val _errorFlow = MutableSharedFlow<Throwable>()
+    val errorFlow get() = _errorFlow.asSharedFlow()
+
+    val sponsorsUiState: StateFlow<SponsorsUiState> =
+        flow { emit(getSponsorsUseCase()) }
+            .map { sponsors ->
+                if (sponsors.isNotEmpty()) {
+                    SponsorsUiState.Sponsors(sponsors.toPersistentList())
+                } else {
+                    SponsorsUiState.Empty
+                }
+            }
+            .catch { throwable ->
+                _errorFlow.emit(throwable)
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = SponsorsUiState.Loading,
+            )
+}
---
+++ feature/home/src/commonMain/kotlin/com/droidknights/app/feature/home/navigation/HomeNavigation.kt
@@ -0,0 +1,24 @@
+package com.droidknights.app.feature.home.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.droidknights.app.core.navigation.MainTabRoute
+import com.droidknights.app.feature.home.HomeRoute
+
+fun NavController.navigateHome(navOptions: NavOptions) {
+    navigate(MainTabRoute.Home, navOptions)
+}
+
+fun NavGraphBuilder.homeNavGraph(
+    padding: PaddingValues,
+    onSessionClick: () -> Unit,
+    onContributorClick: () -> Unit,
+    onShowErrorSnackBar: (throwable: Throwable?) -> Unit,
+) {
+    composable<MainTabRoute.Home> {
+        HomeRoute(padding, onSessionClick, onContributorClick, onShowErrorSnackBar)
+    }
+}
