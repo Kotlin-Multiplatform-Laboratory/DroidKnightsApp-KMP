723b581bbe627995be0aea835a608aec6bcd5224
LEE YOU BIN <youbeene3409@naver.com>
2025-03-22 00:31:51 +0900

    delete open

--- core/domain/src/commonMain/kotlin/com/droidknights/app/core/domain/usecase/GetSponsorsUseCase.kt
+++ core/domain/src/commonMain/kotlin/com/droidknights/app/core/domain/usecase/GetSponsorsUseCase.kt
@@ -5,11 +5,11 @@ import com.droidknights.app.core.model.Sponsor
 import org.koin.core.annotation.Factory
 
 @Factory
-open class GetSponsorsUseCase(
+class GetSponsorsUseCase(
     private val sponsorRepository: SponsorRepository,
 ) {
 
-    open suspend operator fun invoke(): List<Sponsor> =
+    suspend operator fun invoke(): List<Sponsor> =
         sponsorRepository
             .getSponsors()
             .sortedBy { it.grade.priority }
